701b43a8d19e1e2e3ec2fd4475c2d36a94898518
ping <chenchangping@foxmail.com>
2020-03-14 14:26:51 +0000

    增加 CSRF 验证

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -7,15 +7,33 @@ use Illuminate\Http\Request;
 
 class UsersController extends Controller
 {
+	/**
+	 * 用户注册
+	 * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+	 */
     public function create()
     {
     	return view('users.create');
     }
 
+	/**
+	 * 显示用户信息
+	 * @param User $user
+	 * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+	 */
 	public function show(User $user)
 	{
 		return view('users.show', compact('user'));
     }
 
+	public function store(Request $request)
+	{
+		$this->validate($request, [
+			'name' => 'required|unique:users|max:50',
+			'email' => 'required|email|unique:user|max:255',
+			'password' => 'required|confirmed|min:6'
+		]);
 
+		return;
+    }
 }
