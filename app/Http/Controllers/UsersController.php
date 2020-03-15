44d16daac070f541ad4fb2e38abcf5e07564441d
ping <chenchangping@foxmail.com>
2020-03-15 07:00:21 +0000

    登陆时记住我

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -47,12 +47,4 @@ class UsersController extends Controller
 		return redirect()->route('users.show', [$user]);
     }
 
-	public function destroy()
-	{
-		Auth::logout();
-
-		session()->flash('success', '您已成功退出');
-
-		return redirect('login');
-    }
 }
