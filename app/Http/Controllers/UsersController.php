f0e0467f69d84bd0a8c4d23b4cab85ee77dc9566
ping <chenchangping@foxmail.com>
2020-03-22 10:31:54 +0000

    查看用户列表

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -19,6 +19,12 @@ class UsersController extends Controller
 			'only' => ['create'],
 		]);
 	}
+
+	public function index()
+	{
+		$users = User::paginate(10);
+		return view('users.index', compact('users'));
+	}
 	
 	/**
 	 * 用户注册
